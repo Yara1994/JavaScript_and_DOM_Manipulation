19bb42a46a55fdce1f388c47710784810cfa7e6e
Yara1994 <nick.yara1994@gmail.com>
2019-07-21 09:56:50 -0700

    First part done

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,60 @@
 // from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+var tableData = data;
+// console.log(tableData)
+
+// Select the table body
+
+var tbody = d3.select("tbody");
+// console.log(tbody)
+
+// make the table
+
+tableData.forEach((report) => {
+
+    var row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+
+        var cell = row.append("td");
+        cell.text(value);
+
+    });
+
+});
+
+// Select the button
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+    // Select the input element and get the raw HTML node
+
+    var inputElement = d3.select("#datetime");
+    
+    // Get the value property of the input element
+
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+    console.log(filteredData)
+
+    tbody.html(" ")
+
+    // make the table
+
+    filteredData.forEach((report) => {
+
+        var row = tbody.append("tr");
+        Object.entries(report).forEach(([key, value]) => {
+    
+            var cell = row.append("td");
+            cell.text(value);
+    
+        });
+    
+    });
+
+});
+
