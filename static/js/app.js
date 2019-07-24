a2e5833b798bd830700b8b0d262c338c63fadbf7
Yara1994 <nick.yara1994@gmail.com>
2019-07-24 10:50:12 -0700

    Finishing work

--- static/js/app.js
+++ static/js/app.js
@@ -28,23 +28,68 @@ var button = d3.select("#filter-btn");
 
 button.on("click", function() {
 
+    // ===================================================================//
+
     // Select the input element and get the raw HTML node
 
-    var inputElement = d3.select("#datetime");
-    
+    var inputElementDatetime = d3.select("#datetime");
+    var inputElementCity = d3.select("#city");
+    var inputElementState = d3.select("#state");
+    var inputElementCountry = d3.select("#country");
+    var inputElementShape = d3.select("#shape");
+
+
     // Get the value property of the input element
 
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
+    var inputValueDatetime = inputElementDatetime.property("value");
+    // console.log(inputValueDatetime);
 
-    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-    console.log(filteredData)
+    var inputValueCity = inputElementCity.property("value").toLowerCase();
+    // console.log(inputValueCity);
+
+    var inputValueState = inputElementState.property("value");
+    // console.log(inputValueState);
+
+    var inputValueCountry = inputElementCountry.property("value");
+    // console.log(inputValueCountry);
+
+    var inputValueShape = inputElementShape.property("value");
+    // console.log(inputValueShape);
+
+
+    var filteredDatetime = tableData.filter(ufo => ufo.datetime === inputValueDatetime)
+    var filteredCity = tableData.filter(ufo => ufo.city === inputValueCity)
+    var filteredState = tableData.filter(ufo => ufo.state === inputValueState)
+    var filteredCountry = tableData.filter(ufo => ufo.country === inputValueCountry)
+    var filteredShape = tableData.filter(ufo => ufo.shape === inputValueShape)
+
+    if(Boolean(inputValueDatetime)) {
+        tableData = filteredDatetime
+    }
+    tbody.html(" ")
+
+    if(Boolean(inputValueCity)) {
+        tableData = filteredCity
+    }
+    tbody.html(" ")
+    if(Boolean(inputValueState)) {
+        tableData = filteredState
+    }
+    tbody.html(" ")
+    if(Boolean(inputValueCountry)) {
+        tableData = filteredCountry
+    }
+    tbody.html(" ")
+    if(Boolean(inputValueShape)) {
+        tableData = filteredShape
+    } 
+
 
     tbody.html(" ")
 
     // make the table
 
-    filteredData.forEach((report) => {
+    tableData.forEach((report) => {
 
         var row = tbody.append("tr");
         Object.entries(report).forEach(([key, value]) => {
@@ -56,5 +101,29 @@ button.on("click", function() {
     
     });
 
+    populate(tableData)
+
 });
+
+var resetButton = d3.select("#reset-btn")
+
+resetButton.on("click", function() {
+
+    tbody.html(" ");
+    
+    tableData = data;
+
+    tableData.forEach((report) => {
+
+        var row = tbody.append("tr");
+        Object.entries(report).forEach(([key, value]) => {
+    
+            var cell = row.append("td");
+            cell.text(value);
+    
+        });
+    
+    });
+
+})
 
